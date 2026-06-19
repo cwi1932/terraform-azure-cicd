ae9dc24b85a7002b7ca359e05e59fed5f8a95704
Pramod Sasi <pramodsasi@Pramods-MacBook-Air.local>
2026-06-19 18:32:13 +0530

    initial clean terraform repo without provider binaries

---
+++ main.tf
@@ -0,0 +1,72 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-terraform"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# THIS WAS MISSING
+
+resource "azurerm_network_interface" "nic" {
+
+  name                = "nic-terraform"
+
+  location            = azurerm_resource_group.rg.location
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+
+    name                          = "internal"
+
+    subnet_id                     = azurerm_subnet.subnet.id
+
+    private_ip_address_allocation = "Dynamic"
+
+  }
+
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+
+  name                = "vm-terraform"
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  location            = azurerm_resource_group.rg.location
+
+  size                = "Standard_B1s"
+
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+ 
+}
+
---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_version = ">= 1.1.0"
+
+  required_providers {
+
+    azurerm = {
+
+      source  = "hashicorp/azurerm"
+
+      version = "~> 4.0"
+
+    }
+
+  }
+
+}
+
+provider "azurerm" {
+
+  features {}
+
+}
